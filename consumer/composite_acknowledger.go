ffdedfede2b2f0da1a3126debfece35f929643ea
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2025-08-02 23:50:36 +0300

    feat: composite acknowledger

---
+++ consumer/composite_acknowledger.go
@@ -0,0 +1,27 @@
+package consumer
+
+import (
+	"context"
+
+	sqstypes "github.com/aws/aws-sdk-go-v2/service/sqs/types"
+)
+
+type CompositeAcknowledger struct {
+	Rejecter     acknowledger
+	Acknowledger acknowledger
+}
+
+func newCompositeAcknowledger(acknowledger, rejecter acknowledger) *CompositeAcknowledger {
+	return &CompositeAcknowledger{
+		Rejecter:     rejecter,
+		Acknowledger: acknowledger,
+	}
+}
+
+func (c *CompositeAcknowledger) Ack(ctx context.Context, msg sqstypes.Message) error {
+	return c.Acknowledger.Ack(ctx, msg)
+}
+
+func (c *CompositeAcknowledger) Reject(ctx context.Context, msg sqstypes.Message) error {
+	return c.Rejecter.Reject(ctx, msg)
+}
---
+++ consumer/composite_acknowledger_test.go
@@ -0,0 +1,97 @@
+package consumer
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	sqstypes "github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCompositeAcknowledger(t *testing.T) {
+	mockAck := newMockAcknowledger(nil, 0)
+	mockRejecter := newMockAcknowledger(nil, 0)
+
+	ca := newCompositeAcknowledger(mockAck, mockRejecter)
+
+	assert.NotNil(t, ca)
+	assert.Equal(t, mockAck, ca.Acknowledger)
+	assert.Equal(t, mockRejecter, ca.Rejecter)
+}
+
+func TestCompositeAcknowledger_Ack(t *testing.T) {
+	tests := []struct {
+		name        string
+		ackError    error
+		expectError bool
+	}{
+		{
+			name:        "successful acknowledgment",
+			ackError:    nil,
+			expectError: false,
+		},
+		{
+			name:        "acknowledgment fails",
+			ackError:    errors.New("ack failed"),
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockAck := newMockAcknowledger(tt.ackError, 1)
+			mockRejecter := newMockAcknowledger(nil, 0)
+			
+			ca := newCompositeAcknowledger(mockAck, mockRejecter)
+			ctx := context.Background()
+			msg := sqstypes.Message{}
+
+			err := ca.Ack(ctx, msg)
+
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestCompositeAcknowledger_Reject(t *testing.T) {
+	tests := []struct {
+		name        string
+		rejectError error
+		expectError bool
+	}{
+		{
+			name:        "successful rejection",
+			rejectError: nil,
+			expectError: false,
+		},
+		{
+			name:        "rejection fails",
+			rejectError: errors.New("reject failed"),
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockAck := newMockAcknowledger(nil, 0)
+			mockRejecter := newMockAcknowledger(tt.rejectError, 1)
+			
+			ca := newCompositeAcknowledger(mockAck, mockRejecter)
+			ctx := context.Background()
+			msg := sqstypes.Message{}
+
+			err := ca.Reject(ctx, msg)
+
+			if tt.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
